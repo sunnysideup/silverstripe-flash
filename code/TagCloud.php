8ee9c9e39f7ec8c743245300be36029a2baccc1c
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:37:15 +1300

    PATCH: module clean-up

--- code/TagCloud.php
+++ code/TagCloud.php
@@ -2,25 +2,27 @@
 /*
 */
 
-class TagCloud extends ViewableData  {
+class TagCloud extends ViewableData
+{
+    public static $tagCloudItems = array();
 
-	static $tagCloudItems = array();
+    public static $elementID = "flashcontent";
 
-	static $elementID = "flashcontent";
+    public static function addCloudTag($itemName, $style)
+    {
+        $tagCloudItems[] = array(
+            "itemName" => $itemName,
+            "style" => $style
+        );
+    }
 
-	static function addCloudTag($itemName, $style) {
-		$tagCloudItems[] = array(
-			"itemName" => $itemName,
-			"style" => $style
-		);
-	}
-
-	function init() {
-		foreach(self::$tagCloudItems as $itemArray) {
-			$cloudString = '<a style=\''.$itemArray["style"].'\'>'.$itemArray["ItemName"].'</a>';
-		}
-		Requirements::javascript("flash/javascript/swfobjectOlder.js");
-		$customJS = '
+    public function init()
+    {
+        foreach (self::$tagCloudItems as $itemArray) {
+            $cloudString = '<a style=\''.$itemArray["style"].'\'>'.$itemArray["ItemName"].'</a>';
+        }
+        Requirements::javascript("flash/javascript/swfobjectOlder.js");
+        $customJS = '
 			var so = new SWFObject("themes/massolution/images/tagcloud.swf", "tagcloud", "400", "300", "7", "");
 			so.addParam("wmode", "transparent");
 			so.addVariable("tcolor", "0xffffff");
@@ -30,14 +32,8 @@ class TagCloud extends ViewableData  {
 			so.addVariable("tspeed", "100");
 			so.addVariable("tagcloud", "<tags>'.$cloudString.'</tags>");
 			so.write("'.self::$elementID.'");';
-		Requirements::customScript($customJS);
-		$doSet = new DataObjectSet();
-		return $doSet;
-	}
-
-
-
+        Requirements::customScript($customJS);
+        $doSet = new DataObjectSet();
+        return $doSet;
+    }
 }
-
-
-
